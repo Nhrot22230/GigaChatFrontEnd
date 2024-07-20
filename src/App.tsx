bd8f979affac723c10327ae22a22fc8c818896bd
Nhrot22230 <a20213812@pucp.edu.pe>
2024-07-19 23:38:15 -0500

    Probando React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import React, { useState } from "react";
+import React from "react";
+import { User } from "./model/myProps";
 import "./App.css";
-import FetchDataComponent from "./services/ListUsers";
+import { Link, Route, Routes } from "react-router-dom";
+import Login from "./views/Login";
 
 const App: React.FC = () => {
-  const [text, setText] = useState<string>("");
+
+  const user: User = JSON.parse(localStorage.getItem("user") || "null");
 
   return (
-    <>
-      <h1>Users</h1>
-      <label>
-        Search:
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-      </label>
-      <FetchDataComponent search={text} />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <h1>Giga Chat</h1>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        {user? 
+        <div>
+          <p>Welcome {user.nickname}</p>
+          <a href="#chat">Chat</a> 
+        </div>
+        : 
+        <Link to="/login">Login</Link>
+        }
+      </header>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
   );
 };
 
